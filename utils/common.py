ef5623ecf0a275cffd863cae596c22bc7e06ec1a
Orion <net4orion@163.com>
2024-05-06 21:46:44 +0800

    fix: qoute marks

--- utils/common.py
+++ utils/common.py
@@ -5,4 +5,4 @@ def encapsulate(seq: int, message: str):
     return json.dumps({"seq": seq, "message": message})
 
 def fmt_print(data: dict):
-    print(f"seq: {data["seq"]} message: {data["message"]}")
+    print(f"seq: {data['seq']} message: {data['message']}")
