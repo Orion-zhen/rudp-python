e134803ed12b490e136b7e6e818e12f5b57a8e3b
Orion <net4orion@163.com>
2024-05-07 16:20:37 +0800

    feat: primary gbn

--- utils/common.py
+++ utils/common.py
@@ -1,8 +1,27 @@
-import json
+import struct
 
 
-def encapsulate(seq: int, message: str):
-    return json.dumps({"seq": seq, "message": message})
+def get_checksum(data):
+    return sum(data) & 0xFFFFFFFF
+
+def make_pkt(seq_num, payload,stop=False):
+    flag = 1 if stop else 0
+    checksum = get_checksum(payload)
+    return (
+        struct.pack("I", seq_num)
+        + struct.pack("I", checksum)
+        + struct.pack("I", flag)
+        + payload
+    )
+
+
+def de_pkt(pkt):
+    seq_num = struct.unpack("I", pkt[:4])[0]
+    checksum = struct.unpack("I", pkt[4:8])[0]
+    flag = struct.unpack("I", pkt[8:12])[0]
+    payload = pkt[12:]
+    return seq_num, checksum, flag, payload
+
 
 def fmt_print(data: dict):
     print(f"seq: {data['seq']} message: {data['message']}")
